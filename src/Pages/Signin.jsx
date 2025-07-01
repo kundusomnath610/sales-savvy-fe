b29d9837f13a257645d35734bab375473dae60c6
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-01 16:02:56 +0530

    initial commit

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
+export default function Signup() {
 
-export default function Signin() {
+  const [username, setUsername] = useState('')
+  const handleSubmit = () => {
+      axios.get('http://localhost:8080/tester',{
+        params:{'username':username}
+    })
+  }
   return (
-    <div>Signin</div>
+    <>    
+      <form onSubmit = {handleSubmit}>
+          <label>Username:</label>
+          <input type = "text"
+                 value = {username}
+                 onChange = {(e) => setUsername(e.target.value)}>
+          </input>
+          <button type = 'submit'>Send request</button>
+      </form>
+
+    </>
   )
 }
